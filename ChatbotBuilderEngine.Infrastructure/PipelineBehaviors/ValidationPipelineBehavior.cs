cd097b29e785817392d2692dcedd588345e59592
Izzat <work@izzatalsharif.com>
2024-12-06 16:20:34 +0200

    CB-18 Add Infrastructure project

---
+++ ChatbotBuilderEngine.Infrastructure/PipelineBehaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,39 @@
+﻿using ChatbotBuilderEngine.Domain.Shared;
+using FluentValidation;
+using MediatR;
+
+namespace ChatbotBuilderEngine.Infrastructure.PipelineBehaviors;
+
+public class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TResponse : Result
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationPipelineBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var errors = _validators
+                .Select(validator => validator.Validate(request))
+                .SelectMany(validationResult => validationResult.Errors)
+                .Where(validationFailure => validationFailure is not null)
+                .ToList();
+
+            if (errors.Count != 0)
+            {
+                throw new ValidationException(errors);
+            }
+        }
+
+        return await next();
+    }
+}
---
+++ ChatbotBuilderEngine/DependencyInjection/InfrastructureServicesExtension.cs
@@ -0,0 +1,13 @@
+﻿using ChatbotBuilderEngine.Infrastructure.PipelineBehaviors;
+using MediatR;
+
+namespace ChatbotBuilderEngine.DependencyInjection;
+
+public static class InfrastructureServicesExtension
+{
+    public static void AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingPipelineBehavior<,>));
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+    }
+}
