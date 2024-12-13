a423186416454c7692d1618099a10c672405e76c
Izzat <work@izzatalsharif.com>
2024-12-13 19:37:00 +0200

    CB-19 Add Meta value objects

---
+++ ChatbotBuilderEngine.Domain/Graphs/ValueObjects/Meta/InfoMeta.cs
@@ -0,0 +1,26 @@
+﻿using ChatbotBuilderEngine.Domain.Core.Primitives;
+
+namespace ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Meta;
+
+/// <remarks>
+/// Corresponds to Component base class in upper layers.
+/// </remarks>
+public sealed class InfoMeta : ValueObject
+{
+    public int Identifier { get; }
+    public string Name { get; }
+
+    private InfoMeta(int identifier, string name)
+    {
+        Identifier = identifier;
+        Name = name;
+    }
+
+    public static InfoMeta Create(int identifier, string name) => new(identifier, name);
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Identifier;
+        yield return Name;
+    }
+}
---
+++ ChatbotBuilderEngine.Domain/Graphs/ValueObjects/Meta/VisualMeta.cs
@@ -0,0 +1,26 @@
+﻿using ChatbotBuilderEngine.Domain.Core.Primitives;
+
+namespace ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Meta;
+
+/// <remarks>
+/// Corresponds to VisualComponent base class in upper layers.
+/// </remarks>
+public sealed class VisualMeta : ValueObject
+{
+    public float X { get; }
+    public float Y { get; }
+
+    private VisualMeta(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public static VisualMeta Create(float x, float y) => new(x, y);
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return X;
+        yield return Y;
+    }
+}
