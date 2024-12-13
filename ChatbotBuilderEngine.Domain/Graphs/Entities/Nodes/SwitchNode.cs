d17a4daceb3d1589d1dd1bd5723d1eb7ab5325ed
Izzat <work@izzatalsharif.com>
2024-12-13 20:01:44 +0200

    CB-19 Add SwitchNode

---
+++ ChatbotBuilderEngine.Domain/Graphs/Entities/Nodes/SwitchNode.cs
@@ -0,0 +1,101 @@
+﻿using ChatbotBuilderEngine.Domain.Core;
+using ChatbotBuilderEngine.Domain.Graphs.Abstract;
+using ChatbotBuilderEngine.Domain.Graphs.Abstract.Behaviors;
+using ChatbotBuilderEngine.Domain.Graphs.Entities.Ports;
+using ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Data;
+using ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Ids;
+using ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Meta;
+
+namespace ChatbotBuilderEngine.Domain.Graphs.Entities.Nodes;
+
+public sealed class SwitchNode : Node,
+    IInputNode, IEnumNode, IActiveNode, IMultiFlowNode
+{
+    public InputPort<OptionData> InputPort { get; } = null!;
+    public Enum Enum { get; } = null!;
+    public Dictionary<OptionData, FlowLinkId> Bindings { get; } = null!;
+
+    private OptionData? _selectedOption;
+    private readonly Dictionary<FlowLinkId, IFlowNode> _successors = [];
+
+    private SwitchNode(
+        NodeId id,
+        DateTime createdAt,
+        DateTime updatedAt,
+        InfoMeta info,
+        VisualMeta visual,
+        InputPort<OptionData> inputPort,
+        Enum @enum,
+        Dictionary<OptionData, FlowLinkId> bindings)
+        : base(id, createdAt, updatedAt, info, visual)
+    {
+        InputPort = inputPort;
+        Enum = @enum;
+        Bindings = bindings;
+    }
+
+    /// <inheritdoc/>
+    private SwitchNode()
+    {
+    }
+
+    public static SwitchNode Create(
+        NodeId id,
+        DateTime createdAt,
+        DateTime updatedAt,
+        InfoMeta info,
+        VisualMeta visual,
+        InputPort<OptionData> inputPort,
+        Enum @enum,
+        Dictionary<OptionData, FlowLinkId> bindings)
+    {
+        if (bindings.Count != @enum.Options.Count)
+        {
+            throw new DomainException(GraphsDomainErrors.SwitchNode.InvalidBindingsCount);
+        }
+
+        inputPort.EnsureNodeIdIs(id);
+
+        return new SwitchNode(id, createdAt, updatedAt, info, visual, inputPort, @enum, bindings);
+    }
+
+    public IEnumerable<InputPortId> GetInputPortIds()
+    {
+        yield return InputPort.Id;
+    }
+
+
+    public Task ActivateAsync()
+    {
+        _selectedOption = InputPort.GetData();
+        return Task.CompletedTask;
+    }
+
+    public IFlowNode GetSuccessor()
+    {
+        if (_selectedOption is null)
+        {
+            throw new DomainException(GraphsDomainErrors.SwitchNode.HasNotBeenActivated);
+        }
+
+        if (!Bindings.TryGetValue(_selectedOption, out var flowLinkId))
+        {
+            throw new DomainException(GraphsDomainErrors.SwitchNode.OptionNotBound);
+        }
+
+        if (!_successors.TryGetValue(flowLinkId, out var successor))
+        {
+            throw new DomainException(GraphsDomainErrors.SwitchNode.FlowLinkNotBound);
+        }
+
+        return successor;
+    }
+
+    public void Bind(FlowLinkId flowLinkId, IFlowNode node)
+    {
+        if (!_successors.TryAdd(flowLinkId, node))
+        {
+            throw new DomainException(GraphsDomainErrors.SwitchNode.FlowLinkAlreadyBound);
+        }
+    }
+}
--- ChatbotBuilderEngine.Domain/Graphs/GraphsDomainErrors.cs
+++ ChatbotBuilderEngine.Domain/Graphs/GraphsDomainErrors.cs
@@ -36,5 +36,32 @@ public static class GraphsDomainErrors
             "OutputPort.InputPortAlreadySubscribed",
             "This input port is already subscribed to this output port");
     }
+
+    public static class SwitchNode
+    {
+        public static readonly Error InvalidBindingsCount = new(
+            ErrorType.DomainValidation,
+            "SwitchNode.InvalidBindingsCount",
+            "The number of bindings does not match the number of options in the enum");
+
+        public static readonly Error FlowLinkAlreadyBound = new(
+            ErrorType.DomainValidation,
+            "SwitchNode.FlowLinkAlreadyBound",
+            "This flow link is already bound to a successor node");
+
+        public static readonly Error FlowLinkNotBound = new(
+            ErrorType.DomainValidation,
+            "SwitchNode.FlowLinkNotBound",
+            "The selected flow link is not bound to any successor node");
+
+        public static readonly Error HasNotBeenActivated = new(
+            ErrorType.DomainValidation,
+            "SwitchNode.HasNotBeenActivated",
+            "This switch node has not been activated yet");
+
+        public static readonly Error OptionNotBound = new(
+            ErrorType.DomainValidation,
+            "SwitchNode.OptionNotBound",
+            "The selected option is not bound to any flow link");
     }
 }
