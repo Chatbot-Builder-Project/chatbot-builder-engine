c4ff4da80e1195ae94ba5ae5aeeae61724d54ccb
Izzat <work@izzatalsharif.com>
2024-12-13 18:17:18 +0200

    CB-19 Add DomainException

---
+++ ChatbotBuilderEngine.Domain/Core/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using ChatbotBuilderEngine.Domain.Core.Primitives;
+
+namespace ChatbotBuilderEngine.Domain.Core.Exceptions;
+
+public class DomainException : Exception
+{
+    public Error Error { get; }
+
+    public DomainException(Error error) : base(error.Message)
+    {
+        Error = error;
+    }
+}
