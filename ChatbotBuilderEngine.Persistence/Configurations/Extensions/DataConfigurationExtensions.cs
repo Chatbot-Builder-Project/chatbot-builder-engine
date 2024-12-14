5cb28f5141a412eb38a208c9539e98b527fa7ecd
Izzat <work@izzatalsharif.com>
2024-12-14 19:42:52 +0200

    CB-19 Add Data & Meta configuration extensions

---
+++ ChatbotBuilderEngine.Persistence/Configurations/Extensions/DataConfigurationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using ChatbotBuilderEngine.Domain.ValueObjects.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatbotBuilderEngine.Persistence.Configurations.Extensions;
+
+internal static class DataConfigurationExtensions
+{
+    public static void ConfigureTextData<T>(
+        this OwnedNavigationBuilder<T, TextData> builder)
+        where T : class
+    {
+        builder.Property(d => d.Text)
+            .IsRequired();
+    }
+
+    public static void ConfigureOptionData<T>(
+        this OwnedNavigationBuilder<T, OptionData> builder)
+        where T : class
+    {
+        builder.Property(d => d.EnumId)
+            .IsRequired()
+            .ApplyEntityIdConversion();
+
+        builder.Property(d => d.Value)
+            .IsRequired();
+    }
+
+    public static void ConfigureImageData<T>(
+        this OwnedNavigationBuilder<T, ImageData> builder)
+        where T : class
+    {
+        builder.Property(d => d.Url)
+            .IsRequired();
+    }
+}
---
+++ ChatbotBuilderEngine.Persistence/Configurations/Graphs/Extensions/MetaConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Meta;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatbotBuilderEngine.Persistence.Configurations.Graphs.Extensions;
+
+internal static class MetaConfigurationExtensions
+{
+    public static void ConfigureInfoMeta<T>(
+        this OwnedNavigationBuilder<T, InfoMeta> builder)
+        where T : class
+    {
+        builder.Property(d => d.Identifier)
+            .IsRequired();
+
+        builder.Property(d => d.Name)
+            .IsRequired();
+    }
+
+    public static void ConfigureVisualMeta<T>(
+        this OwnedNavigationBuilder<T, VisualMeta> builder)
+        where T : class
+    {
+        builder.Property(v => v.X)
+            .IsRequired();
+
+        builder.Property(v => v.Y)
+            .IsRequired();
+    }
+}
