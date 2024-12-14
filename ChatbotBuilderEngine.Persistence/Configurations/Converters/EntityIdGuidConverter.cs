8c303fd6d1b8ae8b997f327d5652a3da2bc88012
Izzat <work@izzatalsharif.com>
2024-12-14 19:41:17 +0200

    CB-19 Add EntityIdGuidConverter

---
+++ ChatbotBuilderEngine.Persistence/Configurations/Converters/EntityIdGuidConverter.cs
@@ -0,0 +1,15 @@
+﻿using ChatbotBuilderEngine.Domain.Core.Primitives;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace ChatbotBuilderEngine.Persistence.Configurations.Converters;
+
+internal sealed class EntityIdGuidConverter<TId> : ValueConverter<TId, Guid>
+    where TId : EntityId<TId>
+{
+    public EntityIdGuidConverter()
+        : base(
+            id => id.Value,
+            guid => (TId)Activator.CreateInstance(typeof(TId), guid)!)
+    {
+    }
+}
---
+++ ChatbotBuilderEngine.Persistence/Configurations/Extensions/ConversionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using ChatbotBuilderEngine.Domain.Core.Primitives;
+using ChatbotBuilderEngine.Persistence.Configurations.Converters;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatbotBuilderEngine.Persistence.Configurations.Extensions;
+
+public static class ConversionExtensions
+{
+    public static void ApplyEntityIdConversion<TId>(
+        this PropertyBuilder<TId> propertyBuilder)
+        where TId : EntityId<TId>
+    {
+        propertyBuilder.HasConversion(new EntityIdGuidConverter<TId>());
+    }
+}
