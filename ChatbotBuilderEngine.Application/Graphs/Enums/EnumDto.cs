dd1208139838ce8eb6a77998c6505f3edefaad05
Izzat <work@izzatalsharif.com>
2024-12-23 22:06:02 +0200

    CB-24 Add EnumDto with validator

---
+++ ChatbotBuilderEngine.Application/Graphs/Enums/EnumDto.cs
@@ -0,0 +1,8 @@
+﻿using ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Data;
+using ChatbotBuilderEngine.Domain.Graphs.ValueObjects.Meta;
+
+namespace ChatbotBuilderEngine.Application.Graphs.Enums;
+
+public sealed record EnumDto(
+    InfoMeta Info,
+    IReadOnlyList<OptionData> Options);
---
+++ ChatbotBuilderEngine.Application/Graphs/Enums/EnumValidator.cs
@@ -0,0 +1,22 @@
+﻿using ChatbotBuilderEngine.Application.Core.Extensions;
+using ChatbotBuilderEngine.Application.Graphs.Shared.Data;
+using ChatbotBuilderEngine.Application.Graphs.Shared.Metas;
+using FluentValidation;
+
+namespace ChatbotBuilderEngine.Application.Graphs.Enums;
+
+public sealed class EnumValidator : AbstractValidator<EnumDto>
+{
+    public EnumValidator()
+    {
+        RuleFor(x => x.Info)
+            .SetValidator(new InfoMetaValidator());
+
+        RuleFor(x => x.Options)
+            .IsNotEmpty()
+            .IsUnique();
+
+        RuleForEach(x => x.Options)
+            .SetValidator(new OptionDataValidator());
+    }
+}
