d5302b67749b5ee05f09fdb83429d4f5eb39b5d6
Izzat <work@izzatalsharif.com>
2024-12-26 23:21:03 +0200

    CB-24 Register repository services

--- ChatbotBuilderEngine/DependencyInjection/PersistenceServicesExtension.cs
+++ ChatbotBuilderEngine/DependencyInjection/PersistenceServicesExtension.cs
@@ -1,4 +1,7 @@
-﻿using ChatbotBuilderEngine.Application.Core.Abstract;
+﻿using ChatbotBuilderEngine.Application.Chatbots;
+using ChatbotBuilderEngine.Application.Conversations;
+using ChatbotBuilderEngine.Application.Core.Abstract;
+using ChatbotBuilderEngine.Application.Workflows;
 using ChatbotBuilderEngine.Persistence;
 using ChatbotBuilderEngine.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -17,5 +20,8 @@ public static class PersistenceServicesExtension
         });
 
         services.AddScoped<IUnitOfWork, UnitOfWork>();
+        services.AddScoped<IChatbotRepository, ChatbotRepository>();
+        services.AddScoped<IConversationRepository, ConversationRepository>();
+        services.AddScoped<IWorkflowRepository, WorkflowRepository>();
     }
 }
